b8578ca1c1f04b1169ad89c7a2f657f30bb32e31
EasonLin <75676459+Eason023@users.noreply.github.com>
2025-11-24 20:48:40 +0800

    Add Freshman - Second Semester files

---
+++ Freshman - Second Semester/競技程式設計(一)/Finalterm Problem/Problem/submissions/accepted/V_ac_code_113550153.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#define int long long
+using namespace std;
+
+int dp[605][605],h[605],weight,n,ans;
+//dp[i][j] denote for a subsequence end at i-th student, the min. weight for a j length subsequence.
+
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	cin>>n>>weight;
+	for(int i=1;i<=n;i++)
+		for(int j=2;j<=n;j++)
+			dp[i][j]=LLONG_MAX;
+	for(int i=1;i<=n;i++)
+		cin>>h[i];
+	for(int i=1;i<=n;i++){
+		cin>>dp[i][1];
+		if(dp[i][1]<=weight)
+			ans=1;
+	}
+	for(int j=2;j<=n;j++){
+		for(int i=1;i<=n;i++){
+			int tmp=LLONG_MAX;
+			for(int k=1;k<i;k++)
+				if(h[k]<h[i])
+					tmp=min(tmp,dp[k][j-1]);
+			dp[i][j]=(tmp==LLONG_MAX?LLONG_MAX:tmp+dp[i][1]);
+			if(dp[i][j]<=weight)
+				ans=max(ans,j);
+		}
+	}
+	cout<<ans<<'\n';
+}
